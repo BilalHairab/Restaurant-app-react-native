158f6494812b01b38ff06308cb1facb95448c752
Bilal Hairab <42405303+bhairabthirdwayv@users.noreply.github.com>
2022-01-10 13:53:40 +0200

    Implement categorization (with basic UI)

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import ResultsList from '../components/ResultsList'
 import useResults from '../hooks/useResults'
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [searchApi, results, errorMessage] = useResults()
+    // price === '$' || '$$' || '$$$'
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
+        })
+    }
 
     return (
         <View>
@@ -15,6 +22,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Bit Spender" />
         </View>
     )
 }
